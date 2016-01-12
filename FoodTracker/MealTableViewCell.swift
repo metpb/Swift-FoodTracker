865bd1320ff1b47aa5451d01c0009a90fc65cd5a
Prasanna Pegu <prasannapegu@gmail.com>
2016-01-12 14:38:23 +0530

    Added meal list cell

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Prasanna Pegu on 12/01/16.
+//  Copyright © 2016 Prasanna Pegu. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
